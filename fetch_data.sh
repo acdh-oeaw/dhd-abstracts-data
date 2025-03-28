d962bd864329b1fa45e9f5e9b8713b89454111db
csae8092 <p.andorfer@gmail.com>
2025-03-28 13:08:51 +0100

    feat: init commit with fetch-data script and some data

---
+++ fetch_data.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+YEARS=(2016 2017 2018 2019 2020 2021 2022 2023 2024)
+BASE_URL=https://github.com/DHd-Verband/DHd-Abstracts-
+
+for year in "${YEARS[@]}"
+do
+    url=${BASE_URL}${year}/archive/refs/heads/main.zip
+    echo "Downloading data for year: ${year} from ${url}"
+    wget ${url}
+    unzip main.zip -d tmp
+    counter=1
+    find tmp -path "*/XML-files/*.xml" -type f | while read file; do
+        new_name=$(printf "abstract-%d-%05d.xml" "$year" "$counter")
+        mkdir -p data/editions
+        cp "$file" "data/editions/$new_name"
+        ((counter++))
+    done
+    rm -rf tmp
+    rm -rf main.zip
+    
+done
